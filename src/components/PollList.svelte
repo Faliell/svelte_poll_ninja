d4f31ab4142a9d93b8e88ba269be96f5503ba514
Faliell <fabiosw@me.com>
2022-10-24 09:56:13 +0100

    store

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,11 +1,24 @@
 <script>
+    import { onMount, onDestroy } from "svelte";
+    import PollStore from "../stores/PollStore.js"
     import PollDetails from "./PollDetails.svelte";
-    export let polls = [];
+    
+    // export let polls = [];
+
+    // const unsub = PollStore.subscribe(data => {
+    //     polls = data;
+    // })
+
+    // onDestroy(()=> {
+    //     console.log("componet destroyed");
+    //     unsub();
+    // }
+    // )
 
 </script>
 
 <div class="poll-list">
-    {#each polls as poll (poll.id)}
+    {#each $PollStore as poll (poll.id)}
     <div>
         <PollDetails {poll} on:vote />
     </div>        
